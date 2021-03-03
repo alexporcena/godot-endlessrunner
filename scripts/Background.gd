8291c68ec10f5b64eccf9e455960a79ff2040b13
Alex Porcena <79947822+alexporcena@users.noreply.github.com>
2021-03-02 21:19:14 -0300

    Add files via upload

---
+++ scripts/Background.gd
@@ -0,0 +1,13 @@
+extends Sprite
+
+export var velocidade = 1.0
+var largura_inicial = 0.0
+
+func _ready():
+	largura_inicial = texture.get_width() * scale.x
+	
+func _physics_process(delta):
+	position.x = position.x - velocidade
+	
+	if position.x < -largura_inicial:
+		position.x = position.x + largura_inicial * 2
